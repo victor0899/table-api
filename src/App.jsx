58f2e97cd2afa0db4d54a86a9ecdd61907260182
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2024-06-27 19:58:27 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Table from "./components/Table";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Table />
+    </div>
+  );
+}
